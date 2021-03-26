8ca9c63edf8a194908a6f0483a07ff1b60687a5b
VadymSula <sulawados312000@gmail.com>
2021-03-26 15:41:07 +0200

    #2 fix: changed location of capabilities

--- src/main/java/core/InitialDriver.java
+++ src/main/java/core/InitialDriver.java
@@ -1,7 +1,6 @@
 package core;
 
 import configs.PropertiesConfig;
-import enums.Devices;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.remote.MobileCapabilityType;
 import org.apache.log4j.Logger;
@@ -10,7 +9,6 @@ import org.openqa.selenium.remote.DesiredCapabilities;
 import java.net.MalformedURLException;
 import java.net.URL;
 
-
 public class InitialDriver {
     static AndroidDriver driver;
     static DesiredCapabilities cap = new DesiredCapabilities();
@@ -18,34 +16,28 @@ public class InitialDriver {
 
     public static AndroidDriver getDriver() {
         if (driver == null) {
-            driver = initializeDriver();
+            try {
+                driver = initializeDriver();
+                log.info("Driver is started");
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            }
         }
         return driver;
     }
 
-    private static AndroidDriver initializeDriver() {
-        if (PropertiesConfig.getProperty("nameDevice").equals(Devices.REALME6PRO.getType())) {
-            cap.setCapability(MobileCapabilityType.PLATFORM_VERSION, "10.0");
-            cap.setCapability(MobileCapabilityType.NO_RESET, false);
-            cap.setCapability("autoGrandPermission", true);
-            cap.setCapability("appPackage", "se.scmv.belarus");
-            cap.setCapability("appActivity", "se.scmv.belarus.activities.SplashScreenActivity");
-        } else if (PropertiesConfig.getProperty("nameDevice").equals(Devices.REDMINOTE8.getType())) {
-            cap.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
-            cap.setCapability(MobileCapabilityType.PLATFORM_VERSION, "9.0");
-            cap.setCapability(MobileCapabilityType.NO_RESET, false);
-            cap.setCapability("autoGrandPermission", true);
-            cap.setCapability("appPackage", "se.scmv.belarus");
-            cap.setCapability("appActivity", "se.scmv.belarus.activities.SplashScreenActivity");
-        }
+    private static AndroidDriver initializeDriver() throws MalformedURLException {
+        setCapabilities();
+        return new AndroidDriver(new URL("http://0.0.0.0:4723/wd/hub"), cap);
+    }
 
-        AndroidDriver driver = null;
-        try {
-            driver = new AndroidDriver(new URL("http://0.0.0.0:4723/wd/hub"), cap);
-            log.info("Driver is started");
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        }
-        return driver;
+    private static void setCapabilities() {
+        cap.setCapability(MobileCapabilityType.DEVICE_NAME, PropertiesConfig.getProperty("deviceName"));
+        cap.setCapability(MobileCapabilityType.PLATFORM_NAME, PropertiesConfig.getProperty("platformName"));
+        cap.setCapability(MobileCapabilityType.PLATFORM_VERSION, PropertiesConfig.getProperty("platformVersion"));
+        cap.setCapability(MobileCapabilityType.NO_RESET, false);
+        cap.setCapability("autoGrandPermission", true);
+        cap.setCapability("appPackage", PropertiesConfig.getProperty("myAppPackage"));
+        cap.setCapability("appActivity", PropertiesConfig.getProperty("myAppActivity"));
     }
 }
