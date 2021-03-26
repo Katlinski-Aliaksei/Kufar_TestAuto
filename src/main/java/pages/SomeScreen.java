72de844791b573cf3dcb3cc1470efc31ac5f86fc
Katlinski-Aliaksei <70640837+Katlinski-Aliaksei@users.noreply.github.com>
2021-03-26 17:03:40 +0300

    refactoring xpath

--- src/main/java/pages/SomeScreen.java
+++ src/main/java/pages/SomeScreen.java
@@ -7,19 +7,17 @@ import org.openqa.selenium.WebElement;
 public class SomeScreen extends BasePage {
     @AndroidFindBy(id = "se.scmv.belarus:id/login")
     private WebElement LOGIN_BUTTON;
-    @AndroidFindBy(xpath = "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout/android.view.ViewGroup/androidx.viewpager.widget.ViewPager/androidx.recyclerview.widget.RecyclerView/android.widget.FrameLayout/android.widget.ScrollView/android.widget.LinearLayout/android.view.ViewGroup/android.view.ViewGroup/android.widget.ImageView")
-    private WebElement CLOSE_BUTTON;
     @AndroidFindBy(id = "com.google.android.gms:id/cancel")
     private WebElement CANCEL_BUTTON;//кнопка "ничего из перечисленного" всплывающее окно
     @AndroidFindBy(id = "se.scmv.belarus:id/avatar")
     private WebElement AVATAR_BUTTON;
-    @AndroidFindBy(xpath = "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/androidx.recyclerview.widget.RecyclerView/android.widget.FrameLayout[3]/android.widget.TextView")
+    @AndroidFindBy(xpath = "/hierarchy//android.widget.FrameLayout[3]/android.widget.TextView")
     private WebElement LOGOUT_BUTTON;
     @AndroidFindBy(xpath = "//android.widget.ImageButton[@content-desc=\"Перейти вверх\"]")
     private WebElement BACK_BUTTON_PROFILE;
-    @AndroidFindBy(xpath = "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout/android.view.ViewGroup/androidx.viewpager.widget.ViewPager/androidx.recyclerview.widget.RecyclerView/android.widget.FrameLayout/android.widget.ScrollView/android.widget.LinearLayout/android.widget.FrameLayout[1]/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.EditText")
+    @AndroidFindBy(xpath = "/hierarchy//android.widget.FrameLayout[1]//android.widget.EditText")
     private WebElement EMAIL_PATH;
-    @AndroidFindBy(xpath = "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout/android.view.ViewGroup/androidx.viewpager.widget.ViewPager/androidx.recyclerview.widget.RecyclerView/android.widget.FrameLayout/android.widget.ScrollView/android.widget.LinearLayout/android.widget.FrameLayout[2]/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.EditText")
+    @AndroidFindBy(xpath = "/hierarchy//android.widget.FrameLayout[2]//android.widget.EditText")
     private WebElement PASSWORD_PATH;
 
     public SomeScreen(AndroidDriver driver) {
