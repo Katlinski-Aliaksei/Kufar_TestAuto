13c8c61a373b9be98fff7cec60a7a0593ba5c22a
VadymSula <sulawados312000@gmail.com>
2021-03-30 10:09:24 +0300

    #1 add: add swipe

--- src/main/java/elements/Screen.java
+++ src/main/java/elements/Screen.java
@@ -1,6 +1,7 @@
 package elements;
 
 import core.InitialDriver;
+import enums.Direction;
 import io.appium.java_client.TouchAction;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.touch.WaitOptions;
@@ -9,6 +10,7 @@ import org.apache.log4j.Logger;
 import org.openqa.selenium.Dimension;
 
 import java.time.Duration;
+
 
 public class Screen {
     private AndroidDriver driver = InitialDriver.getDriver();
@@ -19,26 +21,42 @@ public class Screen {
         log.info("Page is scrollable");
     }
 
-    public void swipeFromUpToBottom() {
+    public void swipe(Direction dir) {
+        final int ANIMATION_TIME = 200;
         final int PRESS_TIME = 200;
-        int edgeBorder = 10;
+        int edgeBorder = 200;
         PointOption pointOptionStart, pointOptionEnd;
         Dimension dims = driver.manage().window().getSize();
         pointOptionStart = PointOption.point(dims.width / 2, dims.height / 2);
-        pointOptionEnd = PointOption.point(dims.width / 2, dims.height - edgeBorder);
+
+        switch (dir) {
+            case DOWN: // center of footer
+                pointOptionEnd = PointOption.point(dims.width / 2, dims.height - edgeBorder);
+                break;
+            case UP: // center of header
+                pointOptionEnd = PointOption.point(dims.width / 2, edgeBorder);
+                break;
+            case LEFT: // center of left side
+                pointOptionEnd = PointOption.point(edgeBorder, dims.height / 2);
+                break;
+            case RIGHT: // center of right side
+                pointOptionEnd = PointOption.point(dims.width - edgeBorder, dims.height / 2);
+                break;
+            default:
+                throw new IllegalArgumentException("swipeScreen(): dir: '" + dir + "' NOT supported");
+        }
         try {
             new TouchAction(driver)
                     .press(pointOptionStart)
                     .waitAction(WaitOptions.waitOptions(Duration.ofMillis(PRESS_TIME)))
                     .moveTo(pointOptionEnd)
-                    .release()
-                    .perform();
+                    .release().perform();
         } catch (Exception e) {
             System.err.println("swipeScreen(): TouchAction FAILED\n" + e.getMessage());
             return;
         }
         try {
-            Thread.sleep(1000);
+            Thread.sleep(ANIMATION_TIME);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
