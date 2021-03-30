4fbace182ca7169aaabac119e1c48091b76b221d
VadymSula <sulawados312000@gmail.com>
2021-03-30 13:12:23 +0300

    #3 add: add test for favorite sellers

---
+++ src/test/java/tests/favorites/FavoritesSellersTest.java
@@ -0,0 +1,53 @@
+package tests.favorites;
+
+import core.InitialDriver;
+import enums.Credentials;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+import pages.SomeScreen;
+import pages.advertisements.Advert;
+import pages.advertisements.Advertisements;
+import pages.favorites.Favorites;
+
+public class FavoritesSellersTest {
+    Favorites favorites = new Favorites(InitialDriver.getDriver());
+    SomeScreen someScreen = new SomeScreen(InitialDriver.getDriver());
+    Advert advert = new Advert(InitialDriver.getDriver());
+    Advertisements advertisements = new Advertisements(InitialDriver.getDriver());
+
+    @BeforeTest
+    public void logIn() {
+        favorites.signInOrLogin();
+        someScreen.compelledLogIn();
+        someScreen.enterCredentialsAndLogin(Credentials.EMAIL.getType(), Credentials.PASS.getType());
+    }
+
+    @Test
+    public void followingOnSeller() {
+        advertisements.goToAdvertisements();
+        advertisements.goToFirstAdvert();
+        advert.goToSellerInfo();
+        String expectedNameOfSeller = advert.getNameOfSeller();
+        advert.followOnSeller();
+        advert.goBack();
+        advertisements.goToFavorites();
+        favorites.chooseSellersSection();
+
+        String actualNameOfSeller = favorites.getNameOfSeller();
+        Assert.assertEquals(expectedNameOfSeller, actualNameOfSeller);
+    }
+
+    @Test
+    public void unfollowFromSeller() {
+        favorites.unfollowOfSeller();
+
+        Assert.assertTrue(favorites.isNotVisibleSellerInFavorites());
+    }
+
+    @AfterTest
+    public void logOut() {
+        someScreen.logOut();
+    }
+}
